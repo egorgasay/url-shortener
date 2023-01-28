9a5c1b438f8fb73b62a4fb62e1beb77c2cf07bf9
Egor <egorpoletaykin@gmail.com>
2023-01-28 14:20:06 -0500

    increment 12 1.1

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -2,43 +2,30 @@ package handler
 
 import (
 	"bytes"
-	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http/httptest"
 	"testing"
 	"url-shortener/config"
 	"url-shortener/internal/repository"
-	service_mocks "url-shortener/internal/service/mocks"
 )
 
 func TestHandler_GetLinkHandler(t *testing.T) {
-	type mockBehavior func(r *service_mocks.MockIService)
-
 	tests := []struct {
 		name                 string
 		target               string
-		mockBehavior         mockBehavior
 		expectedStatusCode   int
 		expectedResponseHead string
 	}{
 		{
-			name:   "Ok",
-			target: "/zE",
-			mockBehavior: func(r *service_mocks.MockIService) {
-				r.EXPECT().GetLink("IVI").Return(
-					"http://zrnzruvv7qfdy.ru/hlc65i", nil).AnyTimes()
-			},
+			name:                 "Ok",
+			target:               "/zE",
 			expectedStatusCode:   307,
 			expectedResponseHead: "http://zrnzruvv7qfdy.ru/hlc65i",
 		},
 		{
-			name:   "Err",
-			target: "/IVI1",
-			mockBehavior: func(r *service_mocks.MockIService) {
-				r.EXPECT().GetLink("IVI1").Return(
-					"", errors.New("bad url")).AnyTimes()
-			},
+			name:                 "Err",
+			target:               "/IVI1",
 			expectedStatusCode:   400,
 			expectedResponseHead: "",
 		},
@@ -46,11 +33,6 @@ func TestHandler_GetLinkHandler(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			//c := gomock.NewController(t)
-			//defer c.Finish()
-			//
-			//repos := service_mocks.NewMockIService(c)
-			//test.mockBehavior(repos)
 			cfg := &repository.Config{
 				DriverName:     "map",
 				DataSourcePath: "test",
@@ -81,43 +63,21 @@ func TestHandler_GetLinkHandler(t *testing.T) {
 }
 
 func TestHandler_CreateLinkHandler(t *testing.T) {
-	type mockBehavior func(r *service_mocks.MockIService)
-
 	tests := []struct {
 		name                 string
 		inputBody            string
-		mockBehavior         mockBehavior
 		expectedStatusCode   int
 		expectedResponseBody string
 	}{
 		{
-			name:      "Ok",
-			inputBody: "vk.com/gasayminajj",
-			mockBehavior: func(r *service_mocks.MockIService) {
-				r.EXPECT().CreateLink("vk.com/gasayminajj").Return(
-					"BEh6", nil)
-			},
+			name:                 "Ok",
+			inputBody:            "vk.com/gasayminajj",
 			expectedStatusCode:   201,
 			expectedResponseBody: `zE`,
 		},
-		//{
-		//	name:      "already exists",
-		//	inputBody: "vk.com/gasayminajj",
-		//	mockBehavior: func(r *service_mocks.MockIService) {
-		//		r.EXPECT().CreateLink("vk.com/gasayminajj").Return(
-		//			"", gin.Error{Err: errors.New("URL уже существует")})
-		//	},
-		//	expectedStatusCode:   500,
-		//	expectedResponseBody: "",
-		//},
 		{
-			name:      "server error",
-			inputBody: "q",
-			mockBehavior: func(r *service_mocks.MockIService) {
-				r.EXPECT().CreateLink("q").Return(
-					"", gin.Error{Err: errors.New("недопустимый URL")}).
-					AnyTimes()
-			},
+			name:                 "server error",
+			inputBody:            "q",
 			expectedStatusCode:   500,
 			expectedResponseBody: "",
 		},
@@ -125,11 +85,6 @@ func TestHandler_CreateLinkHandler(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			//c := gomock.NewController(t)
-			//defer c.Finish()
-			//
-			//repos := service_mocks.NewMockIService(c)
-			//test.mockBehavior(repos)
 			cfg := &repository.Config{
 				DriverName:     "map",
 				DataSourcePath: "test",
@@ -158,43 +113,21 @@ func TestHandler_CreateLinkHandler(t *testing.T) {
 }
 
 func TestHandler_APICreateLinkHandler(t *testing.T) {
-	type mockBehavior func(r *service_mocks.MockIService)
-
 	tests := []struct {
 		name                 string
 		inputBody            string
-		mockBehavior         mockBehavior
 		expectedStatusCode   int
 		expectedResponseBody string
 	}{
 		{
-			name:      "Ok",
-			inputBody: `{"url":"vk.com/gasayminajj"}`,
-			mockBehavior: func(r *service_mocks.MockIService) {
-				r.EXPECT().CreateLink("vk.com/gasayminajj").Return(
-					"BEh6", nil).AnyTimes()
-			},
+			name:                 "Ok",
+			inputBody:            `{"url":"vk.com/gasayminajj"}`,
 			expectedStatusCode:   201,
 			expectedResponseBody: `{"result":"zE"}`,
 		},
-		//{
-		//	name:      "already exists",
-		//	inputBody: `{"url":"http://zrnzqddy.ru/hlc65i"}`,
-		//	mockBehavior: func(r *service_mocks.MockIService) {
-		//		r.EXPECT().CreateLink("http://zrnzqddy.ru/hlc65i").Return(
-		//			"", gin.Error{Err: errors.New("URL уже существует")})
-		//	},
-		//	expectedStatusCode:   500,
-		//	expectedResponseBody: "",
-		//},
 		{
-			name:      "server error",
-			inputBody: "q",
-			mockBehavior: func(r *service_mocks.MockIService) {
-				r.EXPECT().CreateLink("q").Return(
-					"", gin.Error{Err: errors.New("недопустимый URL")}).
-					AnyTimes()
-			},
+			name:                 "server error",
+			inputBody:            "q",
 			expectedStatusCode:   500,
 			expectedResponseBody: "",
 		},
@@ -202,11 +135,6 @@ func TestHandler_APICreateLinkHandler(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			//c := gomock.NewController(t)
-			//defer c.Finish()
-			//
-			//repos := service_mocks.NewMockIService(c)
-			//test.mockBehavior(repos)
 			cfg := &repository.Config{
 				DriverName:     "map",
 				DataSourcePath: "test",
